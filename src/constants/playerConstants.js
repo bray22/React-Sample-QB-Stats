0698469061b65f888e3e953254af685a8c33a7b3
Ben Ray <bray22@gmail.com>
2022-01-19 09:26:10 -0500

    TruMedia - update token

--- src/constants/playerConstants.js
+++ src/constants/playerConstants.js
@@ -1,2 +1,3 @@
 export const TRUMEDIA_PROJECT_BASE_URL = "https://project.trumedianetworks.com";
-export const API_TOKEN = "665a00a2-97cb-46e3-b9fc-481f61612a4c";
+//export const API_TOKEN = "665a00a2-97cb-46e3-b9fc-481f61612a4c";
+export const API_TOKEN = "c64d5a11-04f3-4086-b442-4929ea8dc960";
